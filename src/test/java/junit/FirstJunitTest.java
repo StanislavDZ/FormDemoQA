9ca2ba04254c4afa9275ec8b9623d295d984219b
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-02-11 22:03:07 +0300

    Done + chk form

--- src/test/java/junit/FirstJunitTest.java
+++ src/test/java/junit/FirstJunitTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -47,6 +48,21 @@ public class FirstJunitTest {
         $("#city").click();
         $(byText("Delhi")).click();
         $("#submit").click();
+
+        //Проверяем заполнение формы
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Stanislav Zaychenko"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("Ashestodust@yandex.ru"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Male"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("9147851961"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("13 April,1987"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("Computer Science, Arts"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports, Reading, Music"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("pic.png"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Taxes, BestCh.St 17"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("NCR Delhi"));
+
+        $(byText("Close")).click();
     }
 }
 
