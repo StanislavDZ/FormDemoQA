08436a6284417b5ba8e22c172290c1c1dbb341e6
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-02-22 21:55:34 +0300

    Done Основы Java+

---
+++ src/test/java/test/HerokuappDragNDrop.java
@@ -0,0 +1,24 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.*;
+
+    public class HerokuappDragNDrop {
+
+        @BeforeAll
+        static void beforeAll() {
+            Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        }
+
+        @Test
+        void shouldCheckDragAndDrop() {
+            open("/drag_and_drop");
+            $("#column-a").dragAndDropTo($("#column-b")).$("header").shouldHave(text("B"));
+            $("#column-b").$("header").shouldHave(text("A"));
+        }
+    }
---
+++ src/test/java/test/SelenideGitWiki.java
@@ -0,0 +1,29 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class SelenideGitWiki {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void checkSoftAssertion() {
+        open("/");                                                                                      //Открыть Github
+        $("[data-test-selector=nav-search-input]").setValue("selenide").pressEnter();                          //Осуществить поиск selenide и перейти на страницу поиска
+        $$(".repo-list li").first().$("a").click();                                                            //Открываем первую ссылку в списке по запросу Selenide
+        $("#repository-container-header").shouldHave(text("Wiki"));                                            //Проверяем наличие Wiki на странице
+        $("#wiki-tab").click();                                                                                //Перейдите в раздел Wiki проекта
+        $("#wiki-pages-box").$(".js-wiki-more-pages-link").click();                                            //Разворачиваем список дополнительных меню
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions")).$(byText("SoftAssertions")).click(); //Проверяем наличие SoftAssertions и сразу переходим на страницу
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class:"));        //Проверяем наличие примера JUnit5
+    }
+}
