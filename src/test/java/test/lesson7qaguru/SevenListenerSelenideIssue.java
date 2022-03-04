ad8494e75280d086962e936fc6afaeb44d0d7574
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-03-04 17:44:16 +0300

    Done Основы Java+

--- src/test/java/test/lesson7qaguru/SevenListenerSelenideIssue.java
+++ src/test/java/test/lesson7qaguru/SevenListenerSelenideIssue.java
@@ -1,17 +1,26 @@
 package test.lesson7qaguru;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SevenListenerSelenideIssue {
 
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://github.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
     @Test
-    void SevenListenerSelenideIssue(){
+    void SevenListenerSelenideIssue() {
         SelenideLogger.addListener("allure", new AllureSelenide());
         //Открыть Гитхаб
         open("https://github.com");
@@ -22,7 +31,6 @@ public class SevenListenerSelenideIssue {
         $(By.partialLinkText("Issues")).click();
         $(withText("Issues")).should(Condition.exist);
 
-        closeWebDriver();
 
     }
 }
