0aa603d1ba9b3c9437c311ee233a0d831056005d
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-03-08 22:24:03 +0300

    1
    Branch 1.0

--- src/test/java/test/lesson7qaguru/SevenWebWebStepsIssue.java
+++ src/test/java/test/lesson7qaguru/SevenWebWebStepsIssue.java
@@ -1,4 +1,5 @@
 package test.lesson7qaguru;
+
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Attachment;
@@ -14,42 +15,48 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
-public class SevenWebStepsIssue {
+public class SevenWebWebStepsIssue {
 
     private static final String URL = "https://github.com";
     private static final String REPOSITORY = "StanislavDZ/FormDemoQA1";
-    private static  final int ISSUE_NUMBER = 1;
+    private static final int ISSUE_NUMBER = 1;
 
     @Step("Open page {url}")
     public void openPage(String URL) {
         open(URL);
     }
+
     @Step("Search repository {repository}")
     public void searchRepository(String repository) {
         $(".header-search-input").click();
         $(".header-search-input").sendKeys(repository);
         $(".header-search-input").submit();
     }
+
     @Step("Open URL {repository}")
     public void openLink(String REPOSITORY) {
         $(By.linkText(REPOSITORY)).click();
     }
+
     @Step("Click menu ISSUES")
     public void clickTabMenuIssues() {
         $(By.partialLinkText("Issues")).click();
     }
+
     @Step("Check number #{number}")
     public void checkNumber(int number) {
-        $(withText("#"+number)).should(Condition.exist);
+        $(withText("#" + number)).should(Condition.exist);
     }
+
     @Attachment(value = "Screenshot", type = "pic/png", fileExtension = "png")
     public byte[] takeScreenshot() {
         return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
+
     @Test
     public void testAnnotatedSteps() {
         addListener("allure", new AllureSelenide());
-        SevenWebStepsIssue steps = new SevenWebStepsIssue();
+        SevenWebWebStepsIssue steps = new SevenWebWebStepsIssue();
         steps.openPage(URL);
         steps.searchRepository(REPOSITORY);
         steps.openLink(REPOSITORY);
---
+++ src/test/java/test/lesson8qaguru/DNDTestTo8lesson.java
@@ -0,0 +1,23 @@
+package test.lesson8qaguru;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DNDTestTo8lesson {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+    }
+
+    @Test
+    void testDragNDrop() {
+        open("/drag_and_drop");
+        $("#column-a").dragAndDropTo($("#column-b")).$("header").shouldHave(text("B"));
+        $("#column-b").$("header").shouldHave(text("A")); sleep(5000);
+    }
+}
