bef5a1b11092a9d034fd7a38048088e484eaa415
StanislavDZ <87750213+StanislavDZ@users.noreply.github.com>
2022-03-09 00:06:22 +0300

    Done Основы Java+ commit branch 3

--- src/test/java/test/lesson7qaguru/SevenWebStepsIssue.java
+++ src/test/java/test/lesson7qaguru/SevenWebStepsIssue.java
@@ -1,5 +1,4 @@
 package test.lesson7qaguru;
-
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Attachment;
@@ -15,48 +14,42 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 
-public class SevenWebWebStepsIssue {
+public class SevenWebStepsIssue {
 
     private static final String URL = "https://github.com";
     private static final String REPOSITORY = "StanislavDZ/FormDemoQA1";
-    private static final int ISSUE_NUMBER = 1;
+    private static  final int ISSUE_NUMBER = 1;
 
     @Step("Open page {url}")
     public void openPage(String URL) {
         open(URL);
     }
-
     @Step("Search repository {repository}")
     public void searchRepository(String repository) {
         $(".header-search-input").click();
         $(".header-search-input").sendKeys(repository);
         $(".header-search-input").submit();
     }
-
     @Step("Open URL {repository}")
     public void openLink(String REPOSITORY) {
         $(By.linkText(REPOSITORY)).click();
     }
-
     @Step("Click menu ISSUES")
     public void clickTabMenuIssues() {
         $(By.partialLinkText("Issues")).click();
     }
-
     @Step("Check number #{number}")
     public void checkNumber(int number) {
-        $(withText("#" + number)).should(Condition.exist);
+        $(withText("#"+number)).should(Condition.exist);
     }
-
     @Attachment(value = "Screenshot", type = "pic/png", fileExtension = "png")
     public byte[] takeScreenshot() {
         return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
-
     @Test
     public void testAnnotatedSteps() {
         addListener("allure", new AllureSelenide());
-        SevenWebWebStepsIssue steps = new SevenWebWebStepsIssue();
+        SevenWebStepsIssue steps = new SevenWebStepsIssue();
         steps.openPage(URL);
         steps.searchRepository(REPOSITORY);
         steps.openLink(REPOSITORY);
